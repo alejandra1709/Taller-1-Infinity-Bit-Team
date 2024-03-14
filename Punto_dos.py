8589b743b547bf807bc9a4d9a13bc46cd27f76d8
alejandra1709 <159057535+alejandra1709@users.noreply.github.com>
2024-03-13 19:37:38 -0500

    Update Punto_dos.py

--- Punto_dos.py
+++ Punto_dos.py
@@ -1,9 +1,11 @@
 a : float = float(input("Ingrese primer numero: "))
 b : float = float(input("Ingrese segundo numero: "))
 c : float = float(input("Ingrese tercer numero: "))
-if a>b and a>c :
-  print("Primer numero es mayor que el segundo y tercer numero")
-elif b>a and b>c :
-  print("Segundo numero es mayor que el primer y tercer numero")
+if a==b and a==c:
+  print("Los tres números son iguales")
+elif a>=b and a>=c :
+  print("El número mayor es ",a)
+elif b>=a and b>=c :
+  print("El número mayor es ",b)
 else :
-  print("Tercer numero es mayor que el primer y segundo numero")
+  print("El número mayor es ",c)
